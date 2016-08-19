1ac93dd3ff42752b20507bf55dead23018468e6b
TomE <tom.eastman.jr@gmail.com>
2016-08-19 14:45:38 -0700

    out of time. final commit. added some more validators

--- apps/pythonbeltapp/views.py
+++ apps/pythonbeltapp/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, HttpResponse, redirect, reverse
 from .models import Trip
 from ..loginreg.models import User
+from django.contrib import messages
 
 def index(request):
     user_id = request.session["id"]
@@ -21,9 +22,17 @@ def add(request):
 
 def create(request):
     user = User.objects.get(id=request.session['id'])
+    description = request.POST["description"]
+    trip_begin=request.POST["trip_begin"]
+    trip_end=request.POST["trip_end"]
+    destination=request.POST["destination"]
 
-    trip = Trip.objects.create(description=request.POST["description"], trip_begin=request.POST["trip_begin"], trip_end=request.POST["trip_end"], destination=request.POST["destination"], user=user)
-    return redirect(reverse('pythonbeltapp:show', kwargs={"trip_id":trip.id}))
+    if len(description) >1 and len(description) >1:
+        trip = Trip.objects.create(description=description, trip_begin=trip_begin, trip_end=trip_end, destination=destination, user=user)
+        return redirect(reverse('pythonbeltapp:show', kwargs={"trip_id":trip.id}))
+    else:
+        messages.add_message(request, messages.SUCCESS, 'Try again')
+        return render(request, 'pythonbeltapp/add.html')
 
 # def update(request, trip_id):
 #     user = User.objects.get(id=request.session['id'])
