ff2a6e969275e11cbf18d75944e7b8c55e29e2c0
TomE <tom.eastman.jr@gmail.com>
2016-08-20 13:29:06 -0700

    added final validators and displaying trips properly

--- apps/pythonbeltapp/views.py
+++ apps/pythonbeltapp/views.py
@@ -2,11 +2,15 @@ from django.shortcuts import render, HttpResponse, redirect, reverse
 from .models import Trip
 from ..loginreg.models import User
 from django.contrib import messages
+from datetime import datetime
 
 def index(request):
     user_id = request.session["id"]
     context = {
-        "trips":Trip.objects.all()
+        "trips":Trip.objects.all()[:5],
+        "me": Trip.objects.filter(user_id=user_id),
+        "me2": Trip.objects.all().filter(travelers=user_id),
+        "notme": Trip.objects.all().exclude(travelers=user_id).exclude(user=user_id)
     }
     return render(request, 'pythonbeltapp/index.html', context)
 
@@ -21,22 +25,24 @@ def add(request):
     return render(request, 'pythonbeltapp/add.html')
 
 def create(request):
-    user = User.objects.get(id=request.session['id'])
-    description = request.POST["description"]
-    trip_begin=request.POST["trip_begin"]
-    trip_end=request.POST["trip_end"]
-    destination=request.POST["destination"]
+    if request.POST["trip_begin"] and request.POST["trip_end"]:
+        user = User.objects.get(id=request.session['id'])
+        description = request.POST["description"]
+        trip_begin = datetime.strptime(request.POST["trip_begin"], '%Y-%m-%d')
+        trip_end = datetime.strptime(request.POST["trip_end"], '%Y-%m-%d')
+        destination=request.POST["destination"]
+        print trip_begin
 
-    if len(description) >1 and len(description) >1:
-        trip = Trip.objects.create(description=description, trip_begin=trip_begin, trip_end=trip_end, destination=destination, user=user)
-        return redirect(reverse('pythonbeltapp:show', kwargs={"trip_id":trip.id}))
+        if len(description) >1 and len(description) >1 and trip_begin > datetime.now() and trip_end > trip_begin:
+            trip = Trip.objects.create(description=description, trip_begin=trip_begin, trip_end=trip_end, destination=destination, user=user)
+            return redirect(reverse('pythonbeltapp:show', kwargs={"trip_id":trip.id}))
     else:
         messages.add_message(request, messages.SUCCESS, 'Try again')
         return render(request, 'pythonbeltapp/add.html')
 
 def update(request, trip_id):
-    user = User.objects.get(id=request.session['id'])
-    trip1 = Trip.objects.get(id=trip_id)
-    trip1.travelers.add(user)
-    trip1.save()
-    return redirect(reverse('pythonbeltapp:show', kwargs={"trip_id":trip1.id}))
+    userid = User.objects.get(id=request.session['id'])
+    selected_trip = Trip.objects.get(id=trip_id)
+    selected_trip.travelers.add(userid)
+    selected_trip.save()
+    return redirect(reverse('pythonbeltapp:show', kwargs={"trip_id":selected_trip.id}))
