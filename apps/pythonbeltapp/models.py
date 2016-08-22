d55b0c576572e1f893177633372cd2d81bc02c46
TomE <tom.eastman.jr@gmail.com>
2016-08-22 10:42:16 -0700

    complete working assignment from my laptop

--- apps/pythonbeltapp/models.py
+++ apps/pythonbeltapp/models.py
@@ -8,5 +8,7 @@ class Trip(models.Model):
     trip_end = models.DateField()
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
-    user = models.ForeignKey("loginreg.User")
-    travelers = models.ManyToManyField("loginreg.User", related_name = 'whatever')
+    creator = models.ForeignKey("loginreg.User", related_name = "creator")
+    travelers = models.ManyToManyField("loginreg.User", related_name = "travelers") # User.objects.get(id=request.session['id']).travelers.all is what the related name "travelers" allows you to do. That would give you all of the trips of the User.id referenced is traveling on
+    def __repr__(self):
+        return self.destination + " " + self.creator.name + " " + str(self.travelers.all())
--- apps/pythonbeltapp/views.py
+++ apps/pythonbeltapp/views.py
@@ -5,16 +5,22 @@ from django.contrib import messages
 from datetime import datetime
 
 def index(request):
-    user_id = request.session["id"]
+    if 'id' not in request.session:
+        return redirect(reverse('loginreg:index'))
+
+    user = User.objects.get(id=request.session["id"])
     context = {
-        "trips":Trip.objects.all()[:5],
-        "me": Trip.objects.filter(user_id=user_id),
-        "me2": Trip.objects.all().filter(travelers=user_id),
-        "notme": Trip.objects.all().exclude(travelers=user_id).exclude(user=user_id)
+        "trips":Trip.objects.all()[:100],
+        "me": (Trip.objects.filter(creator=user) | Trip.objects.filter(travelers__id=user.id)).distinct(),
+        "notme": Trip.objects.all().exclude(creator=user).exclude(travelers__id=user.id),
+        "name": user.name
     }
     return render(request, 'pythonbeltapp/index.html', context)
 
 def show(request, trip_id):
+    if 'id' not in request.session:
+        return redirect(reverse('loginreg:index'))
+
     context = {
         "trip": Trip.objects.get(id=trip_id),
         "users": User.objects.all()
@@ -22,27 +28,35 @@ def show(request, trip_id):
     return render(request, 'pythonbeltapp/destination.html', context)
 
 def add(request):
+    if 'id' not in request.session:
+        return redirect(reverse('loginreg:index'))
+
     return render(request, 'pythonbeltapp/add.html')
 
 def create(request):
+    if 'id' not in request.session:
+        return redirect(reverse('loginreg:index'))
+
     if request.POST["trip_begin"] and request.POST["trip_end"]:
         user = User.objects.get(id=request.session['id'])
         description = request.POST["description"]
         trip_begin = datetime.strptime(request.POST["trip_begin"], '%Y-%m-%d')
         trip_end = datetime.strptime(request.POST["trip_end"], '%Y-%m-%d')
         destination=request.POST["destination"]
-        print trip_begin
 
         if len(description) >1 and len(description) >1 and trip_begin > datetime.now() and trip_end > trip_begin:
-            trip = Trip.objects.create(description=description, trip_begin=trip_begin, trip_end=trip_end, destination=destination, user=user)
+            trip = Trip.objects.create(description=description, trip_begin=trip_begin, trip_end=trip_end, destination=destination, creator=user)
             return redirect(reverse('pythonbeltapp:show', kwargs={"trip_id":trip.id}))
     else:
         messages.add_message(request, messages.SUCCESS, 'Try again')
         return render(request, 'pythonbeltapp/add.html')
 
 def update(request, trip_id):
+    if 'id' not in request.session:
+        return redirect(reverse('loginreg:index'))
+
     userid = User.objects.get(id=request.session['id'])
     selected_trip = Trip.objects.get(id=trip_id)
     selected_trip.travelers.add(userid)
-    selected_trip.save()
-    return redirect(reverse('pythonbeltapp:show', kwargs={"trip_id":selected_trip.id}))
+    # return redirect(reverse('pythonbeltapp:show', kwargs={"trip_id":selected_trip.id}))
+    return redirect(reverse('pythonbeltapp:index'))
